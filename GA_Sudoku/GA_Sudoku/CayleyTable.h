736dbc507cd6614c8aa525c98b30bfd87048ec44
Omer Shapira <info@omershapira.com>
2013-10-21 03:04:28 -0400

    Added TODO hooks for cayley tables

--- GA_Sudoku/GA_Sudoku/CayleyTable.h
+++ GA_Sudoku/GA_Sudoku/CayleyTable.h
@@ -1,5 +1,5 @@
 #define ORDER 3
-
+#define GENESIZE 9
 class CayleyTable
 {
 	int cayleyTablePermutation[ORDER * ORDER];
@@ -21,19 +21,40 @@ public:
 	 * Returns the value of a sudoku table at x,y
 	 */
 	int getbyIndexXY(int x, int y){
-		return cayleyTablePermutation[x] + cayleyTablePermutation[(y % ORDER) * ORDER + y / ORDER] % (ORDER * ORDER);
+		return cayleyTablePermutation[x] + cayleyTablePermutation[(y % ORDER) * ORDER + y / ORDER] % (GENESIZE);
 	}
 	int getbyIndex(int i){
-		return getbyIndexXY(i%9, i/9);
+		return getbyIndexXY(i%GENESIZE, i/GENESIZE);
 	}
 
 	void getFromInput(int *inputBoard){
 
 	}
 
-	float fitnessFunction(){}
+	float fitnessFunction(){
 
-	void crossoverFunction(){}
+		//Count repititions in the smapled seciton, raise by 2^-n.
+	}
 
-	void mutationFunction(){}
+	void crossoverFunction(){
+
+		//TODO: Figure out how to sample;
+
+		int gene1[GENESIZE]; //get it from somewhere
+		int gene2[GENESIZE]; //get it from somewhere
+
+		int newGene[GENESIZE];
+
+		for (int i = 0; i < GENESIZE; ++i)
+		{
+			newGene[gene2[i]] = gene1[i]; //Permute 1 by 2 
+		}
+
+		//TODO: Now generate the new sudoku board from getByIndexXY.
+	}
+
+	void mutationFunction(){
+	//TODO add noise, swap columns
+
+	}
 };
